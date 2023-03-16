25b8cad72a72a197a8e9092890d28d4a802a563a
Carla Montano <cm.emprendiendo@gmail.com>
2023-03-15 22:52:00 -0500

    add: initial setup

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -14,17 +14,32 @@ def index(request):
 
 def entry(request, title):
     return render(request, 'encyclopedia/entry.html', {
-        'title': title.capitalize(),
+        'title': title.lower(),
         'entry': util.get_entry(title) 
     })
 
 def search(request):
     if request.method == "POST":
-        query = request.POST['q'].capitalize()
+        query = request.POST['q'].lower()
         entry = util.get_entry(query)
         if entry is not None:
             return render(request,'encyclopedia/entry.html', {
                 "title": query,
                 'entry': entry
             })
-        
+        else:
+            entries = util.list_entries()
+            resultList = []
+            for entry in entries:
+                if query in entry.lower():
+                    resultList.append(entry)
+            return render(request, 'encyclopedia/search.html', {
+                'resultList': resultList
+            })
+
+def add_entry(request):
+    if request.method == "GET":
+        return render(request, "encyclopedia/add.html", {
+
+    })
+
